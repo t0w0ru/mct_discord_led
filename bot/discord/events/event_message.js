1df1bce8effaf37b6b7df625797977158bfac9a9
t0w0ru <52724219+t0w0ru@users.noreply.github.com>
2021-07-20 18:12:03 +0200

    final fix

--- bot/discord/events/event_message.js
+++ bot/discord/events/event_message.js
@@ -1,58 +1,52 @@
-const discord = require ('discord.js');
-const colors = require ('colors');
-const log = require ('../log.js');
-const moment = require ('moment');
+const moment = require('moment');
+const discord = require('discord.js');
+const colors = require('colors');
+const log = require('../log.js');
 
 async function run(bot, message) {
-    if((message.author.bot) || (message.channel.type == "dm"))
-        return;
+    if (message.author.bot) return;
+    if (message.channel.type == "dm") return;
     let member = await message.guild.members.fetch(message.author.id);
-    if(member == undefined) {
-        let err = new discord.MessageEmbed()
+    if (member == undefined) {
+        let e2 = new discord.MessageEmbed()
             .setTitle("internal error occured!")
             .setDescription("couldn't fetch member")
-        message.channel.send(err)
-        return;
+        message.channel.send(e2)
+        return
     }
-        
+
     const args = message.content.split(" ").slice(1);
     const cmd = message.content.toLowerCase().split("").slice(bot.cache.configs.cfg.prefix.length).join("").split(" ")[0];
     let command;
 
-    if(bot.commands.has(cmd)) {
+    if (bot.commands.has(cmd)) {
         command = bot.commands.get(cmd);
-    } else { 
+    } else {
         command = bot.commands.get(bot.aliases.get(cmd));
-    }
-    if(command) {
-        if(command.config.enabled) {
-            if(bot.cache.configs.cfg.blocks.includes(message.author.id))
-                return;
-            if(bot.functions.check_channel(command, message.channel.id)) {
-                log.log_date_time(` HANDLER `.magenta + `${message.author.tag}`.grey + ` called command `.green + `${command.config.name}`.grey);
-                command.run(bot, message, args, discord, moment);
-            } else {
-                if(message.deletable)
-                    message.delete();
-            }
-            
-        } else if (!command.config.enabled) {
+    };
+
+    if (command) {
+        if (command.config.enabled) {
+            log.log_date_time(` HANDLER `.magenta + `${message.author.tag}`.grey + ` called command `.green + `${command.config.name}`.grey)
+            command.run(bot, message, args, discord, moment);
+
+        } else if (command.config.enabled == false) {
             let e2 = new discord.MessageEmbed()
                 .setTitle("command disabled")
                 .setDescription("\`" + bot.cache.configs.cfg.prefix + command.config.name + "\` disabled")
             message.channel.send(e2)
-        }
-    } else {
-        let embed = new discord.MessageEmbed()
-            .setTitle("command not found")
-            .setDescription("please check your spelling.")
-        message.channel.send(embed)
-    }
-}    
 
+        } else {
+            let e2 = new discord.MessageEmbed()
+                .setTitle("command broken")
+                .setDescription("\`" + bot.cache.configs.cfg.prefix + command.config.name + "\` seems to be broken, by either a corrupted config or a corrupted file")
+            message.channel.send(e2)
+        }
+    }
+};
 module.exports = {
     config: {
-        name: "message",
+        name: "message", 
         enabled: true
     },
     run: run
