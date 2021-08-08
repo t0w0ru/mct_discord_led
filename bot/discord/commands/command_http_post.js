ff0899185f63381e8578525ff22803bb3dfbdfd0
t0w0ru <52724219+t0w0ru@users.noreply.github.com>
2021-08-08 09:53:02 +0200

    update config + post

--- bot/discord/commands/command_http_post.js
+++ bot/discord/commands/command_http_post.js
@@ -1,14 +1,17 @@
 const colors = require('colors');
 const log = require('../log');
 const fetch = require('node-fetch');
+var fetch2 = require('fetch-retry')(fetch)
 
 async function run(bot, message, args, discord, moment) {
 
     let mode = args[0].toUpperCase()
-    let col = args[1].toUpperCase()
-    const _body = '{ MODE: ' + `${mode}`+ ', COLOR: ' + `${col}` + ' }';
+    let col = args[1].toUpperCase();
 
-    fetch('https://ptsv2.com/t/u0yw8-1626867561/post', {
+    const _body = '{ "MODE": ' + '"' + `${mode}`+ '"' + ', "COLOR": ' + '"' + `${col}` + '"' + ' }';
+    
+    fetch2('http://' + bot.cache.configs.cfg.ip + '/body', {
+    // fetch('https://ptsv2.com/t/u0yw8-1626867561/post', {
         method: 'POST',
         body:  _body
     })
@@ -17,12 +20,21 @@ async function run(bot, message, args, discord, moment) {
     .then(body => {
         let e = new discord.MessageEmbed()
             .setTitle("Successfully posted")
-            .setDescription(`Response:  ${body}`)
+            .setDescription(`${body}`)
             .addField("MODE: ", `\`\`\`prolog\n${mode}\n\`\`\``)
             .addField("COLOR: ", `\`\`\`prolog\n${col}\n\`\`\``)
             .setColor("#00FF00")
         message.channel.send(e);
+    }) 
+    .catch(err => {
+        let e1 = new discord.MessageEmbed()
+        .setTitle("error!")
+        .setDescription("Couldn't connect!")
+        .addField("Internal error:",  `\`\`\`prolog\n${err}\n\`\`\``)
+        .setColor("#34363C")
+    message.channel.send(e1);
     })
+
     
     function checkStatus(res) {
         if (res.ok) { // res.status >= 200 && res.status < 300
